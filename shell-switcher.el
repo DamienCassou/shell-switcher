ed8d10994c5911e31b1470fc1ef502147f23848b
Damien Cassou <damien.cassou@gmail.com>
2012-06-30 08:44:27 +0200

    Improve documentation of shell-switcher
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- shell-switcher.el
+++ shell-switcher.el
@@ -26,9 +26,9 @@
 ;; Boston, MA 02111-1307, USA.
 ;;
 ;;
-;;; Description:
+;;; commentary:
 ;;
-;; After you add the following line to your emacs init file you will
+;; After you add the following line to your Emacs init file you will
 ;; switch easily between shell buffers:
 ;;
 ;; (require 'shell-switcher)
@@ -49,7 +49,7 @@
 ;; standard window managers, the list of shell buffers is arranged so
 ;; that recently accessed ones are at the beginning of the list.
 ;;
-;;; Code:
+;;; code:
 ;;
 ;; The following code uses the "sswitcher" prefix for all "private"
 ;; functions (and an addition dash "-") and the "shell-switcher" prefix
@@ -64,7 +64,6 @@
 
 (defcustom shell-switcher-new-shell-function 'shell-switcher-make-eshell
   "This variable references a function used to create new shells.
-
 The function must take 0 arguments and return a newly created
 shell buffer. `shell-switcher-make-shell' and
 `shell-switcher-make-eshell' are possible functions."
@@ -73,46 +72,58 @@ shell buffer. `shell-switcher-make-shell' and
 
 (defun shell-switcher-make-shell ()
   "Ensure the creation of a new `shell'.
-
-This function is to be used as value for `shell-switcher-new-shell-function'."
+This function is to be used as value for
+`shell-switcher-new-shell-function'."
   (shell (generate-new-buffer-name "*shell*")))
 
 (defun shell-switcher-make-eshell ()
   "Ensure the creation of a new `eshell'.
-
-This function is to be used as value for `shell-switcher-new-shell-function'."
+This function is to be used as value for
+`shell-switcher-new-shell-function'."
   (eshell t))
 
-(defun sswitcher--current ()
+(defun sswitcher--most-recent ()
+  "Return the most recently accessed shell."
   (rswitcher-current sswitcher-ring))
 
-(defun sswitcher--current-shell-valid-p ()
-  ;; we could potentially add the fact that the shell is still
-  ;; accepting commands, but I don't know how to that for all possible
-  ;; shells. And I'm not sure it's a good idea either :-)
-  (buffer-live-p (sswitcher--current)))
+(defun sswitcher--most-recent-shell-valid-p ()
+  "Check that the most recently created shell can still be accessed."
+  (buffer-live-p (sswitcher--most-recent)))
 
 (defun sswitcher--clean-buffers ()
   "Remove all shell buffers until we find a valid one."
-  (while (and (sswitcher--shell-exist-p)
-	      (not (sswitcher--current-shell-valid-p)))
+  (while (and (not (rswitcher-empty-p sswitcher-ring))
+	      (not (sswitcher--most-recent-shell-valid-p)))
     (rswitcher-delete-current sswitcher-ring)))
 
 (defun sswitcher--shell-exist-p ()
+  "Check that there is at least one valid shell to switch to."
+  (sswitcher--clean-buffers)
   (not (rswitcher-empty-p sswitcher-ring)))
 
 (defun sswitcher--in-shell-buffer-p ()
+  "Check that the current buffer is a shell buffer."
   (rswitcher-memq sswitcher-ring (current-buffer)))
 
 (defun sswitcher--new-shell ()
+  "Create and display a new shell.
+
+This function uses `shell-switcher-new-shell-function' to decide
+what kind of shell to create."
   (rswitcher-add sswitcher-ring (funcall shell-switcher-new-shell-function))
   (sswitcher--display-shell-buffer))
 
 (defun sswitcher--no-more-shell-buffers ()
-  (if (y-or-n-p "No more buffers, create new one?? ")
+  "Propose to create a new shell as there is no more to switch to."
+  (if (y-or-n-p "No more buffers, create new one? ")
       (sswitcher--new-shell)))
 
 (defun sswitcher--prepare-for-fast-key ()
+  "Set a keymap so that one can switch buffers by pressing 1 key.
+The key to be pressed to continue switching buffers is the last
+key of the most recent key sequence. See
+`shell-switcher-switch-buffer' for more information. When this
+key is pressed, calls `sswitcher-switch-partially'."
   (let* ((repeat-key (event-basic-type last-input-event))
 	 (repeat-key-str (format-kbd-macro (vector repeat-key)))
 	 (message (format "Type %s again to continue switching"
@@ -127,14 +138,23 @@ This function is to be used as value for `shell-switcher-new-shell-function'."
     (message message)))
 
 (defun sswitcher--display-shell-buffer ()
-  (sswitcher--clean-buffers)
+  "Display the most recently accessed shell buffer."
   (if (sswitcher--shell-exist-p)
-      (switch-to-buffer (sswitcher--current))
+      (switch-to-buffer (sswitcher--most-recent))
     (message "No shell buffer to display")))
 
 (defun shell-switcher-switch-buffer ()
+  "Provide fast access to all shell buffers.
+Switch to the most recently accessed shell buffer that is not the
+current one. Pressing the last key of the key sequence that call
+this command will result in switching to the next shell buffer :
+for example, if `C-'' is bound to this command, repeatedly
+pressing `'' (quote) will let the user visit all shell
+buffers (this is actually done by `sswitcher-switch-partially'.
+
+If there is no shell buffer or if the only shell buffer is the
+current buffer, propose the creation of a new shell buffer."
   (interactive)
-  (sswitcher--clean-buffers)
   (if (or (not (sswitcher--shell-exist-p))
 	  (and (= (rswitcher-length sswitcher-ring) 1)
 	       (sswitcher--in-shell-buffer-p)))
@@ -145,6 +165,11 @@ This function is to be used as value for `shell-switcher-new-shell-function'."
     (sswitcher--prepare-for-fast-key)))
 
 (defun sswitcher-switch-partially ()
+  "Switch to the next most recently accessed buffer.
+
+This function is indirectly called by
+`shell-switcher-switch-buffer' after pressingthe last key of the
+most recent key sequence."
   (sswitcher--clean-buffers)
   (if (< (rswitcher-length sswitcher-ring) 2)
       (sswitcher--no-more-shell-buffers)
@@ -152,16 +177,12 @@ This function is to be used as value for `shell-switcher-new-shell-function'."
     (sswitcher--display-shell-buffer)))
 
 (defun shell-switcher-new-shell ()
+  "Unconditionaly create and display a new shell buffer."
   (interactive)
   (sswitcher--new-shell))
 
-(defun shell-switcher-switch-or-new-shell ()
-  (interactive)
-  (sswitcher--clean-buffers)
-  (if (and (sswitcher--shell-exist-p)
-	   (not (sswitcher--in-shell-buffer-p)))
-      (sswitcher--display-shell-buffer)
-    (sswitcher--new-shell))
-  (sswitcher--prepare-for-fast-key))
+(provide 'shell-switcher)
 
 (provide 'shell-switcher)
+
+;;; shell-switcher.el ends here
